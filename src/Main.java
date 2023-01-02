983ade19313df8e12661a742cc703b88d281009e
AnatoliyVasyuta <2004anatoliy@gmail.com>
2023-01-02 15:01:13 +0200

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("антипод Біг Бена");
+        //широта
+        double latitude = 51.50067;
+        System.out.println(latitude);
+        // довгота
+        double longitude = -0.12457000000001;
+        System.out.println(longitude);
+
+    }
+}
